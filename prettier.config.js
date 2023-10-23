a1a44de4a1755f5e00bc3af694ac1ccbb0dbaae6
SanderFrakking <s.d.frakking@gmail.com>
2023-10-23 17:35:18 +0200

    .

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,11 @@
 // prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
 
 /** @type {import("prettier").config} */
-const config =	{
-	trailingComma:"es5",
-	tabWidth: 3,
-	singleQuote: true,
-	printWidth: 300,
-	bracketSameLine: true,
+const config = {
+   trailingComma: 'es5',
+   tabWidth: 4,
+   singleQuote: true,
+   printWidth: 300,
+   bracketSameLine: true,
 };
 module.exports = config;
