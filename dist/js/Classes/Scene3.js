153bae35ff7a6c5da1f17f892ef554d310929eee
bred <97704993+BredCodes@users.noreply.github.com>
2023-11-01 11:47:26 +0100

    Update Scene3.js

--- dist/js/Classes/Scene3.js
+++ dist/js/Classes/Scene3.js
@@ -11,12 +11,25 @@ class Scene3 extends Phaser.Scene {
     create() {
         this.add.image(0,0, "background").setOrigin(0).setDepth(0);
 
-        let playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "play_button").setDepth(1);
-
+        const playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "play_button").setDepth(1);
         playButton.setInteractive();
 
-        playButton.on("pointerup", ()=>{
-            this.scene.start('playGame');
+        const clickedImage = this.input.activePointer.texture.key;
+        playButton.on('pointerdown', ()=>{
+
+            if(clickedImage === 'play_button')
+            {
+                console.log("clicked")
+                this.scene.start('bootGame');
+            }
+        });
+
+        playButton.on('pointerover', ()=>{
+
+            if(clickedImage === 'play_button')
+            {
+                console.log("hover")
+            }
         })
     }
 }
