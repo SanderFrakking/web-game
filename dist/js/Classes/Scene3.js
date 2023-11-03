5898605cb3444fe1ff8c494e917f40575bfd6d4b
SanderFrakking <s.d.frakking@gmail.com>
2023-11-03 11:30:50 +0100

    Update Scene3.js

--- dist/js/Classes/Scene3.js
+++ dist/js/Classes/Scene3.js
@@ -1,19 +1,18 @@
 class Scene3 extends Phaser.Scene {
-    constructor() {
-        super('menuGame')
-    }
+  constructor() {
+    super('menuGame');
+  }
 
-    preload() {
-        this.load.image('play_button', './assets/images/Startbutton.png')
-    }
+  preload() {
+    this.load.image('play_button', './assets/images/Startbutton.png');
+  }
 
-    create() {
+  create() {
+    const playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, 'play_button');
+    playButton.setInteractive();
 
-        const playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, "play_button")
-        playButton.setInteractive();
-
-        playButton.on('pointerdown', ()=>{
-            this.scene.start('bootGame');
-        })
-    }
+    playButton.on('pointerdown', () => {
+      this.scene.start('bootGame');
+    });
+  }
 }
